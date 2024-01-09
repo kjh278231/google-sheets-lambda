397608abfd011253c69e19ca2d56c98ce92e6ea3
kjh278231 <kjh9609@gmail.com>
2024-01-09 22:55:27 +0900

    관통 테스트용 수정

--- handler.py
+++ handler.py
@@ -1,11 +1,19 @@
-import numpy as np
+def hello(event, context):
+    try:
+        print(event)
+        print(context)
+        param = event.body.detailParams
+        name = param.name.value
+        number = param.number.value
+        return {
+        "version":"2.0",
+        "template":
+            {"outputs":[{"simpleText":{"text":"hello I'm "+name +": "+number}}]}
+        }
+    except:
+        return {
+        "version":"2.0",
+        "template":
+            {"outputs":[{"simpleText":{"text":"오류가 발생했습니다."}}]}
+        }       
 
-def main(event, context):
-    a = np.arange(15).reshape(3, 5)
-
-    print("Your numpy array:")
-    print(a)
-
-
-if __name__ == "__main__":
-    main('', '')
