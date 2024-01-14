a7983a587681c9022035fe3eac027b53004ed1a9
kjh278231 <kjh9609@gmail.com>
2024-01-14 22:53:12 +0900

    number validation 추가

--- handler.py
+++ handler.py
@@ -13,27 +13,28 @@ def hello(event, context):
         param = json.loads(event["body"])["action"]["params"]
         name = param["name"]
         number = param["number"]
+        number = str.replace(number,"-","")
+        number = str.replace(number,".","")
         ins_person_id = param["channel"]
 
         # json 파일이 위치한 경로를 값으로 줘야 합니다.
         json_file_path = "wired-torus-351301-ad2ef6a9e623.json"
         gc = gspread.service_account(json_file_path)
-        spreadsheet_url = "https://docs.google.com/spreadsheets/d/1SZRkaAGJQM7BWFw3Vxj_LetIBimhAhpgPhMjqmq17Uw/edit#gid=0"
+        spreadsheet_url = "https://docs.google.com/spreadsheets/d/1InUZrKBUz6lnY9xqOSxms660txnx1nCawbaUeitV8kA/edit#gid=1805506674"
         doc = gc.open_by_url(spreadsheet_url)
 
-        worksheet = doc.worksheet("제출 명단")
+        worksheet = doc.worksheet("테스트총계")
 
         all_Data = worksheet.get_all_values()
         if(check_duplicated_data(all_Data,name,number)):
             last_row = len(all_Data) + 1
-            currentTime = datetime.datetime.now().date
             range = 'A'+ str(last_row) + ':D' + str(last_row)
             date_now = str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day) + "-" + str(datetime.datetime.now().year)
             worksheet.update(range,[[date_now,name,number,ins_person_id]])
             return {
                 "version":"2.0",
                 "template":
-                    {"outputs":[{"simpleText":{"text":name+"님 신청해주셔서 감사합니다. 빠른 시간 내에 응답드리도록 하겠습니다!"}}]}
+                    {"outputs":[{"simpleText":{"text":"신청해주셔서 감사합니다. 빠른 시간 내에 응답드리도록 하겠습니다!"}}]}
             }
         else:
             return {
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import gspread
+# import gspread
+import re
 import datetime
 
 def check_duplicated_data(all_Data,name,number):
@@ -8,21 +9,35 @@ def check_duplicated_data(all_Data,name,number):
     return True
 
 # json 파일이 위치한 경로를 값으로 줘야 합니다.
-json_file_path = "wired-torus-351301-ad2ef6a9e623.json"
-gc = gspread.service_account(json_file_path)
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1SZRkaAGJQM7BWFw3Vxj_LetIBimhAhpgPhMjqmq17Uw/edit#gid=0"
-doc = gc.open_by_url(spreadsheet_url)
+# json_file_path = "wired-torus-351301-ad2ef6a9e623.json"
+# gc = gspread.service_account(json_file_path)
+# spreadsheet_url = "https://docs.google.com/spreadsheets/d/1InUZrKBUz6lnY9xqOSxms660txnx1nCawbaUeitV8kA/edit#gid=1805506674"
+# doc = gc.open_by_url(spreadsheet_url)
 
-worksheet = doc.worksheet("제출 명단")
-ins_person_id = "주인계정"
-name = "김지훈"
-number = "01062598538"
+# worksheet = doc.worksheet("WI총계(현물+선물)")
+# ins_person_id = "주인계정"
+# name = "김지훈"
+# number = "01062598538"
 
-all_Data = worksheet.get_all_values()
-if(check_duplicated_data(all_Data,name,number)):
-    last_row = len(all_Data) + 1
-    currentTime = datetime.datetime.now()
-    range = 'A'+ str(last_row) + ':D' + str(last_row)
-    worksheet.update(range,[["2014-01-13",name,number,ins_person_id]])
-else:
-    print('Fail...')
+# all_Data = worksheet.get_all_values()
+# print(all_Data)
+
+# if(check_duplicated_data(all_Data,name,number)):
+#     last_row = len(all_Data) + 1
+#     currentTime = datetime.datetime.now()
+#     range = 'A'+ str(last_row) + ':D' + str(last_row)
+#     worksheet.update(range,[["2014-01-13",name,number,ins_person_id]])
+# else:
+#     print('Fail...')
+
+def validatePhoneNumber(phoneNumber):
+    newPhoneNumber = str.replace(phoneNumber,"-","")
+    fianlPhoneNumber = str.replace(newPhoneNumber,".","")
+    patter_first = r"010([0-9]{8})"
+    if(re.findall(patter_first,fianlPhoneNumber)!= [] or len(fianlPhoneNumber)>=9):
+        return True,fianlPhoneNumber;
+    return False,"";
+
+result,number = validatePhoneNumber("010.1234.5678")
+print(result)
+print(number)
